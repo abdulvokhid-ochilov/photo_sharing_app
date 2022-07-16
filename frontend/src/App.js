0c1a70e63d4fa967c5640473c6f1bad3dcd896d3
Abdulvokhid <mr.ochilov1997@gmail.com>
2022-07-16 22:17:15 +0900

    frontend packages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,15 @@
 import React from "react";
+import { Routes, Route, useNavigate } from "react-router-dom";
+import Login from "./components/Login";
+import Home from "./containers/Home";
 
 const App = () => {
-  return <div className="text-3xl font-bold underline">App</div>;
+  return (
+    <Routes>
+      <Route path="login" element={<Login />} />
+      <Route path="/*" element={<Home />} />
+    </Routes>
+  );
 };
 
 export default App;
