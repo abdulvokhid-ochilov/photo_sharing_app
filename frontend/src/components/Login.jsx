d7b99582a56704e9a05dee99a4944c8e84e12ab3
Abdulvokhid <mr.ochilov1997@gmail.com>
2022-07-17 12:31:03 +0900

    Login page

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,7 +1,58 @@
 import React from "react";
+import { GoogleLogin } from "@react-oauth/google";
+import { useNavigate } from "react-router-dom";
+import jwt_decode from "jwt-decode";
+
+import video from "../assets/share.mp4";
+import logo from "../assets/logowhite.png";
+import { client } from "../client";
 
 const Login = () => {
-  return <div>Login</div>;
+  const navigate = useNavigate();
+
+  const handleLogin = ({ clientId, credential }) => {
+    localStorage.setItem("user", JSON.stringify(credential));
+
+    const { name, picture } = jwt_decode(credential);
+    const doc = {
+      _id: clientId,
+      _type: "user",
+      userName: name,
+      image: picture,
+    };
+
+    console.log(doc);
+    client.createIfNotExists(doc).then(() => {
+      navigate("/", { replace: true });
+    });
+  };
+  return (
+    <div className="flex justify-start items-center flex-col h-screen">
+      <div className="relative w-full h-full">
+        <video
+          src={video}
+          type="video/mp4"
+          loop
+          controls={false}
+          muted
+          autoPlay
+          className="w-full h-full object-cover"
+        />
+        <div className="absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0    bg-blackOverlay">
+          <div className="p-5">
+            <img src={logo} width="130px" alt="logo" />
+          </div>
+
+          <div className="shadow-2xl">
+            <GoogleLogin
+              onSuccess={(response) => handleLogin(response)}
+              onError={() => console.log("error")}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
